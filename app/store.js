c8308a83dd0ccbafc2bca0183fabb70fed4bfd30
harshitsingh45 <harshitsingh45@gmail.com>
2022-08-22 14:29:26 +0530

    added middleware

--- app/store.js
+++ app/store.js
@@ -1,12 +1,15 @@
 const configureStore = require('@reduxjs/toolkit').configureStore 
+const { getDefaultMiddleware } = require('@reduxjs/toolkit');
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
+const logger = require('redux-logger').createLogger()
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 module.exports = store;
